76fff586f9db882c26550a6ed17246f502338b8c
kevin <kvallejop@fi.uba.ar>
2025-04-24 04:36:28 -0300

    Estructura inicial, parseo de argumentos.

---
+++ src/start-server.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from lib.parser import parser
+
+def main():
+    args = parser.parse_server()
+
+    if args.verbose:
+        print("[INFO] Verbose mode ON")
+    elif args.quiet:
+        print("[INFO] Quiet mode ON")
+
+    print(f"[INFO] Server listeneing in IP: {args.host}, PORT:{args.port} using protocol {args.protocol}")
+
+if __name__ == "__main__":
+    main()
