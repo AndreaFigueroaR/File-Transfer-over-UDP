3e16bb78dcce10bab01012d860db5c421319a71c
Leti <leticia.figueroa.r07@gmail.com>
2025-04-26 20:58:39 -0300

    refactor(upload): now use the destinated class to clien logic: Client

--- src/upload.py
+++ src/upload.py
@@ -1,29 +1,14 @@
 from lib.parser import parser
 from lib.client import Client
 
-# MAX_FILE_SIZE = 5 #MB
-
-# class FileCheckError(Exception):
-#     """Error en validación de archivo previo al envío."""
-#     pass
-
-UPLOAD = 'U'
 
 def main(args):
     args = parser.parse_upload()
     display_mode(args)
-    
-    client = Client(UPLOAD, args.protocol)
+
+    client = Client(args.host, args.port, args.protocol)
     client.upload(args.src)
 
-# metodo del client
-# def check_file_exists_or_fail(source_file_path):
-#     if not os.path.exists(source_file_path):
-#         raise FileCheckError(f"Nonexistent file {source_file_path}")
-
-#     file_size = os.path.getsize(source_file_path)
-#     if file_size > (MAX_FILE_SIZE * 1024 * 1024):
-#         raise FileCheckError("Upload invalido a causa de violación de tamaño")
 
 def display_mode(args):
     if args.verbose:
@@ -32,6 +17,7 @@ def display_mode(args):
         print("[INFO] Quiet mode ON")
     print(
         f"[INFO] Uploading file '{args.src}' as '{args.name}' to {args.host}:{args.port} using protocol {args.protocol}")
+
 
 if __name__ == "__main__":
     main()
