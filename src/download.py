6312d15d8ea712257e8cadaeafb553d03d600500
Leti <leticia.figueroa.r07@gmail.com>
2025-04-26 20:52:05 -0300

    refactor(download): moving header into Client class

--- src/download.py
+++ src/download.py
@@ -1,15 +1,14 @@
 from lib.parser import parser
-from lib.server import Server
 from lib.client import Client
- 
-DOWNLOAD = 'D'
 
-def main():
+
+def main(args):
     args = parser.parse_download()
     display_mode(args)
 
-    client = Client(DOWNLOAD, args.protocol)
+    client = Client(args.host, args.port, args.protocol)
     client.download(args.src)
+
 
 def display_mode(args):
     if args.verbose:
@@ -18,6 +17,7 @@ def display_mode(args):
         print("[INFO] Quiet mode ON")
     print(
         f"[INFO] Downloading file '{args.name}' from {args.host}:{args.port} using protocol {args.protocol}")
+
 
 if __name__ == "__main__":
     main()
