43a0fcd988b0a1879968eb7d32ab2379b63206fc
Leti <leticia.figueroa.r07@gmail.com>
2025-04-26 16:46:50 -0300

    refactor(serverRDT): separting logic to mett_client (that means, doing handshaking and cheking some msgs)

---
+++ src/lib/server_rdt.py
@@ -0,0 +1,41 @@
+import socket
+from handshake.server_handshaker import ServerHandshaker
+from lib.selective_repeat import SelectiveRepeat
+from stop_and_wait import StopAndWait
+
+PROT_SR = "sr"
+UPLOAD ='U'
+DOWNLOAD ='D'
+
+HANDSHAKE_SUCCESS = 0
+HANDSHAKE_FAILURE = -1
+
+class ServerRDT: 
+    def __init__(self, addr):
+        self.client_addr = addr
+        self.peer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.protocol = None
+        self.client_num_seq = None
+        self.num_seq = 0
+
+    def meet_client(self, client_data, server_prot_type) -> str:
+        handshaker = ServerHandshaker(self.client_addr, self.num_seq)
+        self.client_num_seq, client_prot_type, app_data = handshaker.handshake(client_data, self.peer)#podría alnzar una excepción
+        if client_prot_type != server_prot_type:
+            raise ValueError(f"Tipo de protocolo inválido: {client_prot_type}. Expected {server_prot_type}")
+        self.protocol = self._init_protocol()
+        return app_data
+
+    def send(self, data):
+        self.protocol.send(data)
+
+    def receive(self):
+        return self.protocol.recieve()
+
+    def stop(self):
+        self.protocol.stop()
+    
+    def _init_protocol(self, prot_type):
+        if prot_type == PROT_SR: 
+            return SelectiveRepeat(self.peer)
+        return StopAndWait(self.peer)
