3660e2466268dba0c4f0007fd760d107d1217f26
Leti <leticia.figueroa.r07@gmail.com>
2025-04-26 16:57:07 -0300

    refactor(upload,download): now use class client. still pending to catch exction in case of server disconnection or shutdown

--- src/download.py
+++ src/download.py
@@ -1,67 +1,23 @@
 from lib.parser import parser
-import random
-import socket
-import time
-
-SERVER_IP = "127.0.0.1"
-SERVER_PORT = 12345
-TIME_OUT = 0.1
-TAM_BUFFER = 1024
-
+from lib.server import Server
+from lib.client import Client
+ 
+DOWNLOAD = 'D'
 
 def main():
     args = parser.parse_download()
+    display_mode(args)
+
+    client = Client(DOWNLOAD, args.protocol)
+    client.download(args.src)
+
+def display_mode(args):
     if args.verbose:
         print("[INFO] Verbose mode ON")
     elif args.quiet:
         print("[INFO] Quiet mode ON")
-
     print(
         f"[INFO] Downloading file '{args.name}' from {args.host}:{args.port} using protocol {args.protocol}")
-
-    run(args)
-
-
-def formated_first_pkt(seq, args) -> str:
-    return f"{seq}|D|{args.protocol}|{args.name}"
-
-
-def try_send_hand_shake_msg(sock, packet, serv_addr):
-    sock.sendto(packet.encode(), serv_addr)
-    try:
-        data_handshake, serv_addr = sock.recvfrom(TAM_BUFFER)
-    except socket.timeout:
-        try_send_hand_shake_msg(sock, packet, serv_addr)
-        time.sleep(1)
-
-    serv_seq, ack = data_handshake.decode().split("|", 1)
-    return (serv_seq, ack, serv_addr)
-
-
-def send_first_hand_shake_msg(sock, seq, args):
-    print(f"expecting {seq}")
-    serv_seq, ack, serv_addr = try_send_hand_shake_msg(
-        sock, formated_first_pkt(
-            seq, args), (parser.SERVER_IP, parser.SERVER_PORT))
-    while int(ack) != seq:  # reenviando cada que se recibe corrupto
-        serv_seq, ack = try_send_hand_shake_msg(sock, seq, args, serv_addr)
-        print(
-            f"[Error]: some error has ocurred: receive ack {int(ack)}, expecting {seq}. Trying to connect the server again")
-    print("logramos")
-    return serv_seq, serv_addr
-
-
-def send_second_hand_shake_msg(sock, serv_seq, serv_addr):
-    sock.sendto(str(serv_seq).encode(), serv_addr)
-
-
-def run(args):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(TIME_OUT)
-    seq = random.randint(1, 100)
-    serv_seq, serv_addr = send_first_hand_shake_msg(sock, seq, args)
-    send_second_hand_shake_msg(sock, serv_seq, serv_addr)
-
 
 if __name__ == "__main__":
     main()
--- src/upload.py
+++ src/upload.py
@@ -1,36 +1,29 @@
 from lib.parser import parser
-import socket
-import os
+from lib.client import Client
 
-TIME_OUT = 0.1
-MAX_FILE_SIZE = 5 #MB
+# MAX_FILE_SIZE = 5 #MB
+
+# class FileCheckError(Exception):
+#     """Error en validación de archivo previo al envío."""
+#     pass
+
+UPLOAD = 'U'
 
 def main(args):
     args = parser.parse_upload()
+    display_mode(args)
+    
+    client = Client(UPLOAD, args.protocol)
+    client.upload(args.src)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.settimeout(TIME_OUT)
+# metodo del client
+# def check_file_exists_or_fail(source_file_path):
+#     if not os.path.exists(source_file_path):
+#         raise FileCheckError(f"Nonexistent file {source_file_path}")
 
-        server_ip, server_port = args.host, args.port
-
-        file_name = args.name #nombre del archivo como se guardará en el servidor
-        source_file_path = args.src # path (local) del archivo que se subirá
-        try:
-            # De este metodo se debería salir del programa (excepción o bien valor de error retornado)
-            check_file_characteristics(source_file_path)
-        except:
-        # handshake independiente al modo de recuperaciónde errores
-        # comunicación durante del handshake siempre estilo stop and wait
-        cli_channel_add = upload_handshake(sock, server_ip, server_port, file_name, source_file_path)
-
-def upload_handshake(sock, server_ip, server_port, file_name, source_file_path):    
-    first_chunk = (int(0).to_bytes(ID_SIZE, byteorder='big')
-                    + UPLOAD.to_bytes(CLIENT_METHOD_SIZE, byteorder='big')
-                    + file_size.to_bytes(FILE_SIZE, byteorder='big')
-                    + name.ljust(CHUNK_SIZE - ID_SIZE - FILE_SIZE - CLIENT_METHOD_SIZE, '\0').encode('utf-8'))
-    sock.sendto(first_chunk, (server_ip, server_port))
-
-###################### secondary functions ######################
+#     file_size = os.path.getsize(source_file_path)
+#     if file_size > (MAX_FILE_SIZE * 1024 * 1024):
+#         raise FileCheckError("Upload invalido a causa de violación de tamaño")
 
 def display_mode(args):
     if args.verbose:
@@ -39,20 +32,6 @@ def display_mode(args):
         print("[INFO] Quiet mode ON")
     print(
         f"[INFO] Uploading file '{args.src}' as '{args.name}' to {args.host}:{args.port} using protocol {args.protocol}")
-    
-def check_file_characteristics(source_file_path) -> int:
-    # chequeo de que el path existe y obtener el largo del path
-    if not os.path.exists(source_file_path):
-        # el printeo mediante una entidad que se encargue de logear los errores
-        # dicha entidad consideraría el nivel de verbose
-        print('ERROR: archivo inexistente')
-        #no puedo hacer un exit porque tengo recursos reservados
-        return -1
-
-    file_size = os.path.getsize(source_file_path)
-    if file_size > (MAX_FILE_SIZE * 1024 * 1024):
-        print('ERROR: Upload invalido a causa de violación de tamaño')
-        return -1
 
 if __name__ == "__main__":
     main()
