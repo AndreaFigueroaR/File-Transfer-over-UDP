b524cccb6714d33bb7f3efa6756fe99fba888f2f
Leti <leticia.figueroa.r07@gmail.com>
2025-04-26 21:00:41 -0300

    feat(protocol_arq): base class for a protocol agnostic as to whether it is server or sender

---
+++ src/lib/protocol_arq.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+TIMEOUT = 1
+TAM_BUFFER = 1024
+
+
+class ProtocolARQ:
+    
+    def __init__(self, socket_peer, local_seq, remote_seq):
+        self.time_out.settimeout(TIMEOUT)
+        self.socket = socket_peer
+        self.local_seq = local_seq
+        self.remote_seq = remote_seq
+        #tupla con address y port de con quien me debería de estar comunicando
+        #self.adress = adress
+
+    @abstractmethod
+    def _recv_segment():
+        pass
+    # No tiene mucho sentido el método unitario de _send_segment
+    # si solo se envia 1 segmento enotnces no podríamos aplicar la
+    # lógica de enviar varios consecutivamente (solo tedría sentido para stop
+    # and wait)
+
+    @abstractmethod
+    def send():
+        pass
+
+    @abstractmethod
+    def _send_segment():
+        pass
