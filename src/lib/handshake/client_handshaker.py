8c0d8a74926e8bfa97204072d4e71203bd1c554d
Leti <leticia.figueroa.r07@gmail.com>
2025-04-26 23:14:30 -0300

    refactor(client_handshaker): relabeling client socket

--- src/lib/handshake/client_handshaker.py
+++ src/lib/handshake/client_handshaker.py
@@ -13,19 +13,19 @@ class ClientHandshaker:
         self.srv_addr = addr
         self.num_seq = num_seq
 
-    def handshake(self, client_type, client_prot_type, file_path, srv_skt):
+    def handshake(self, client_type, client_prot_type, file_path, skt):
         srv_num_seq = self._send_first_handshake_msg(
-            srv_skt, client_type, client_prot_type, file_path)
-        self._send_second_handshake_msg(srv_skt, srv_num_seq)
+            skt, client_type, client_prot_type, file_path)
+        self._send_second_handshake_msg(skt, srv_num_seq)
         return srv_num_seq
 
     def _send_first_handshake_msg(
-            self, srv_skt, client_type, client_prot_type, file_path):
+            self, skt, client_type, client_prot_type, file_path):
         packet = self._formatted_client_info(
             client_type, client_prot_type, file_path)
-        srv_num_seq, ack = self._try_first_handshake_msg(srv_skt, packet)
+        srv_num_seq, ack = self._try_first_handshake_msg(skt, packet)
         while self.num_seq != int(ack):  # forwarding while its corrupted
-            srv_num_seq, ack = self._try_first_handshake_msg(srv_skt, packet)
+            srv_num_seq, ack = self._try_first_handshake_msg(skt, packet)
             print(
                 f"[Error]: ack received {ack}. Expected{self.num_seq}. Trying to connect the server again")
         return srv_num_seq
@@ -42,8 +42,8 @@ class ClientHandshaker:
                 continue
             srv_num_seq, ack = data_handshake.decode().split("|", 1)
             return srv_num_seq, ack
-        raise ConnectionError("Maximum number of handshake attempts reached")
+        raise ConnectionError("[Error]: maximum number of attempts to send message was reached")
 
     def _formatted_client_info(
             self, client_type, client_prot_type, file_path) -> str:
-        return f"{self.num_seq}|{client_type}|{client_prot_type}|{file_path}"
+        return f"{self.num_seq}|{client_prot_type}|{file_path}|{client_type}"
