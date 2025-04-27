877069288c8e2adb7d9becfa9f58e8e1407982c8
Leti <leticia.figueroa.r07@gmail.com>
2025-04-26 23:13:54 -0300

    style(hola): delete drafts

--- src/hola.py
+++
@@ -1,6 +0,0 @@
-# from lib.server import Server
-from lib.handshake.server_handshaker import ServerHandshaker
-k = ServerHandshaker(("127.0.0.1","9000"),0)
-# from lib.server_rdt import ServerRDT
-# i = Server("127.0.0.1","9000","sr")
-# j = ServerRDT(("127.0.0.1","9000"))
