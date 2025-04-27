975d9e3030ff532860b4a04266dab68319ade2f9
Leti <leticia.figueroa.r07@gmail.com>
2025-04-27 01:42:48 -0300

    fix(client_handshaker): now the handshake returns a int (so you can increment the srv number of sequence

--- src/lib/handshake/client_handshaker.py
+++ src/lib/handshake/client_handshaker.py
@@ -13,16 +13,16 @@ class ClientHandshaker:
         self.srv_addr = addr
         self.num_seq = num_seq
 
-    def handshake(self, client_type, client_prot_type, file_path, skt):
+    def handshake(self, client_type, client_prot_type, file_path, skt, file_name):
         srv_num_seq = self._send_first_handshake_msg(
-            skt, client_type, client_prot_type, file_path)
+            skt, client_type, client_prot_type, file_path, file_name)
         self._send_second_handshake_msg(skt, srv_num_seq)
-        return srv_num_seq
+        return int(srv_num_seq), self.srv_addr
 
     def _send_first_handshake_msg(
-            self, skt, client_type, client_prot_type, file_path):
+            self, skt, client_type, client_prot_type, file_path, file_name):
         packet = self._formatted_client_info(
-            client_type, client_prot_type, file_path)
+            client_type, client_prot_type, file_path, file_name)
         srv_num_seq, ack = self._try_first_handshake_msg(skt, packet)
         while self.num_seq != int(ack):  # forwarding while its corrupted
             srv_num_seq, ack = self._try_first_handshake_msg(skt, packet)
@@ -30,14 +30,14 @@ class ClientHandshaker:
                 f"[Error]: ack received {ack}. Expected{self.num_seq}. Trying to connect the server again")
         return srv_num_seq
 
-    def _send_second_handshake_msg(self, srv_skt, srv_num_seq):
-        srv_skt.sendto(str(srv_num_seq).encode(), self.srv_addr)
+    def _send_second_handshake_msg(self, skt, srv_num_seq):
+        skt.sendto(str(srv_num_seq).encode(), self.srv_addr)
 
-    def _try_first_handshake_msg(self, srv_skt, packet):
+    def _try_first_handshake_msg(self, skt, packet):
         for _ in range(NUM_ATTEMPS):
             try:
-                srv_skt.sendto(packet.encode(), self.srv_addr)
-                data_handshake, self.srv_addr = srv_skt.recvfrom(TAM_BUFFER)
+                skt.sendto(packet.encode(), self.srv_addr)
+                data_handshake, self.srv_addr = skt.recvfrom(TAM_BUFFER)
             except socket.timeout:
                 continue
             srv_num_seq, ack = data_handshake.decode().split("|", 1)
@@ -45,5 +45,5 @@ class ClientHandshaker:
         raise ConnectionError("[Error]: maximum number of attempts to send message was reached")
 
     def _formatted_client_info(
-            self, client_type, client_prot_type, file_path) -> str:
-        return f"{self.num_seq}|{client_prot_type}|{file_path}|{client_type}"
+            self, client_type, client_prot_type, file_path, file_name) -> str:
+        return f"{self.num_seq}|{client_prot_type}|{file_path}|{file_name}|{client_type}"
