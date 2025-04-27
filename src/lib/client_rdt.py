d44c55f858b7f029622f7b5729ccad266246fe90
Leti <leticia.figueroa.r07@gmail.com>
2025-04-26 23:10:51 -0300

    fix(client_rdt): fixing bad returnings (no expected order), also relabeling the client socket

--- src/lib/client_rdt.py
+++ src/lib/client_rdt.py
@@ -3,13 +3,14 @@ from lib.handshake.client_handshaker import ClientHandshaker
 from lib.selective_repeat import SelectiveRepeat
 from lib.stop_and_wait import StopAndWait
 
+TIME_OUT = 0.1
 PROT_SR = "sr"
-
 
 class ClientRDT:
     def __init__(self, server_addr):
         self.srv_addr = server_addr
-        self.srv_skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.skt.settimeout(TIME_OUT)
         self.protocol = None
         self.srv_num_seq = None
         self.num_seq = 0
@@ -17,7 +18,7 @@ class ClientRDT:
     def start(self, prot_type, client_type, file_path) -> str:
         handshaker = ClientHandshaker(self.srv_addr, self.num_seq)
         self.srv_num_seq = handshaker.handshake(
-            client_type, prot_type, file_path, self.srv_skt)
+            client_type, prot_type, file_path, self.skt)
         self.protocol = self._init_protocol(prot_type)
 
     def send(self, data):
@@ -32,5 +33,5 @@ class ClientRDT:
     def _init_protocol(self, prot_type):
         if prot_type == PROT_SR:
             return SelectiveRepeat(
-                self.srv_skt, self.num_seq, self.srv_num_seq)
-        return StopAndWait(self.srv_skt, self.num_seq, self.srv_num_seq)
+                self.skt, self.num_seq, self.srv_num_seq)
+        return StopAndWait(self.skt, self.num_seq, self.srv_num_seq)
