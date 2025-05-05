5c0cd8b3359ff370aa9f6cdced50e3761e772dbc
Jesabel Pugliese <jesabelpugliese004@gmail.com>
2025-05-04 22:31:02 -0300

    fix: timeout on stop-and-wait

--- src/lib/protocols/stop_and_wait.py
+++ src/lib/protocols/stop_and_wait.py
@@ -7,33 +7,33 @@ FIN = 2
 
 class StopAndWait(ProtocolARQ):
     def send(self, data):
-        total_len = len(data)
-        self._print_if_verbose(f"Data size to send: {total_len}")
+        self._print_if_verbose(f"Data size to send: {len(data)}")
         data_chunks = self._split_data(data)
         sn = FIRST_SN
         for chunk in data_chunks:
             self._send_data_chunk(sn, chunk)
             sn = 1 - sn
-        self._send_data_chunk(sn, bytes())
+        self._send_data_chunk(FIN, bytes())
 
     def _send_data_chunk(self, sn, chunk):
         for _ in range(NUM_ATTEMPS):
             try:
                 self._send_segment(sn, chunk)
 
-                if not chunk:
-                    self._eof_sender_handshake()
-                    return
-
                 ack = self._recv_ack()
                 if ack == sn:
                     self._print_if_verbose(f"{IND}Received expected ACK: {ack}")
                     return
-                else: 
-                    self._print_if_verbose(f"{IND}Received unexpected ACK: {ack}")
+                if ack == FIN:
+                    self._print_if_verbose("Reach End-Of-Data.")
+                    return
+                self._print_if_verbose(f"{IND}Received unexpected ACK: {ack}")
             except socket.timeout:
+                if sn == FIN:
+                    self._print_if_verbose("Reach End-Of-Data.")
+                    return
                 self._print_if_verbose("TIMEOUT: Try again")
-        raise ConnectionError("Failed to send segment after multiple attempts")
+        raise RuntimeError("Failed to send segment after multiple attempts")
 
     def receive(self):
         bytes_received = 0
@@ -42,36 +42,24 @@ class StopAndWait(ProtocolARQ):
         while True:
             try:
                 sn, payload = self._recv_segment()
-                if not payload:
-                    self._eof_receiver_handshake()
+
+                # TODO: refactor. En lugar de usar un ACK distinto de 0 y 1, 
+                # podríamos agregar una flag en el header (seteada en 1 si es
+                # el ultimo segmento) y comparar sn con la flag.
+                if sn == FIN:
+                    self._print_if_verbose("Reach End-Of-Data.")
+                    self._send_ack(FIN)
                     return data
+
                 if sn == expected_sn:
+                    self._print_if_verbose(f"Expected SN {sn}")
                     data.extend(payload)
                     bytes_received += len(payload)
                     expected_sn = 1 - expected_sn
                 else:
                     self._print_if_verbose(f"Duplicate SN {sn}, re-acked but not re-appended")
                 self._send_ack(sn)
-                self._print_if_verbose(f"Payload bytes received: {bytes_received}")
+                self._print_if_verbose(f"Payload size: {len(payload)}")
+                self._print_if_verbose(f"Total payload bytes received: {bytes_received}")
             except socket.timeout:
                 self._print_if_verbose("TIMEOUT: Try again")
-    
-    def _eof_receiver_handshake(self):
-        while True:
-            try:
-                self._send_ack(FIN)
-                ack = self._recv_ack()
-                if ack == FIN:
-                    return
-            except socket.timeout:
-                self._print_if_verbose("TIMEOUT on End-Of-File handshake: Try again")
-    
-    def _eof_sender_handshake(self):
-        while True:
-            try:
-                ack = self._recv_ack()
-                if ack == FIN:
-                    self._send_ack(FIN)
-                    return
-            except socket.timeout:
-                self._print_if_verbose("TIMEOUT on End-Of-File handshake: Try again")
