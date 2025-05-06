68b8d7155362287402554530d0d63b32534731bc
Leti <leticia.figueroa.r07@gmail.com>
2025-05-06 03:06:33 -0300

    feat(protocol_arq): adding two basic functions to code and decode generic flags.

--- src/lib/protocols/protocol_arq.py
+++ src/lib/protocols/protocol_arq.py
@@ -15,35 +15,11 @@ class ProtocolARQ:
     def send(self, data: bytes):
         pass
 
-    def _send_ack(self, ack):
-        ack_b = ack.to_bytes(1, byteorder='big')
-        self.socket.sendto(ack_b, self.address)
-
-    def _send_segment(self, segment_sn, data_chunk: bytes):
-        self._print_if_verbose(f"{IND}{DELIM}")
-        self._print_if_verbose(f"{IND}Segment size to send: {1 + len(data_chunk)}")
-        
-        seq_num_byte = segment_sn.to_bytes(1, byteorder='big')
-        segment = seq_num_byte + data_chunk
-        self.socket.sendto(segment, self.address)
 
     @abstractmethod
     def receive(self, max_data_size) -> bytearray:
         pass
 
-    def _recv_ack(self):
-        segment, _ = self.socket.recvfrom(1)
-        ack = int.from_bytes(segment[0:1], byteorder='big')
-        return ack
-
-    def _recv_segment(self):
-        segment, _ = self.socket.recvfrom(1 + DATA_CHUNK_SIZE)
-        seq_num = int.from_bytes(segment[0:1], byteorder='big')
-        payload = segment[1:]
-        
-        self._print_if_verbose(DELIM)
-        self._print_if_verbose(f"Segment size recieved: {len(segment)}")
-        return seq_num, payload
 
     def _split_data(self, data: bytes):
         data_chunks = []
@@ -54,3 +30,20 @@ class ProtocolARQ:
     def _print_if_verbose(self, msg):
         if self.is_verbose:
             print(msg)
+
+
+    def _bool_to_byte(self,flag: bool) -> bytes:
+        """
+        Convierte un booleano a un byte:
+        True  → b'\x01'
+        False → b'\x00'
+        """
+        return (1).to_bytes(1, byteorder='big') if flag else (0).to_bytes(1, byteorder='big')
+
+    def _byte_to_bool(self,b: bytes) -> bool:
+        """
+        Convierte un byte de longitud 1 de vuelta a booleano:
+        b'\x01' → True
+        b'\x00' → False
+        """
+        return bool(b[0])
