fd9c061189e7ab38b3f9e6550cb8617f4d308e63
Leti <leticia.figueroa.r07@gmail.com>
2025-05-07 03:52:28 -0300

    refactor(download,upload,start-server): now use debug for verbose or not verbose prints
    Co-authored-by: josValentin-fiuba <josValentin-fiuba@users.noreply.github.com>

--- src/download.py
+++ src/download.py
@@ -2,11 +2,12 @@ from lib.parser import parser
 from lib.client.client import Client
 import lib.debug as debug
 
+
 def main():
     args = parser.parse_download()
     is_verbose = True if args.verbose else False
     debug.verbose = is_verbose
-    client = Client(args.host, args.port, args.protocol, is_verbose)
+    client = Client(args.host, args.port, args.protocol)
     client.download(args.dst, args.name)
 
 
--- src/start-server.py
+++ src/start-server.py
@@ -2,11 +2,12 @@ from lib.parser import parser
 from lib.server.server import Server
 import lib.debug as debug
 
+
 def main():
     args = parser.parse_server()
     is_verbose = True if args.verbose else False
     debug.verbose = is_verbose
-    server = Server(args.host, args.port, args.protocol, args.storage, is_verbose)
+    server = Server(args.host, args.port, args.protocol, args.storage)
     server.accept_clients()
 
 
