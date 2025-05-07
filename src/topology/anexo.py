ccd968fdadf82b46518bb61e4ba5012b0a655f49
Jesabel Pugliese <jesabelpugliese004@gmail.com>
2025-05-07 03:32:31 -0300

    feat: add topology for IP fragmentation

--- src/topology/anexo.py
+++ src/topology/anexo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from mininet.net import Mininet
-from mininet.node import Controller, OVSController, Host
+from mininet.node import Controller, OVSController
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink
@@ -9,30 +9,35 @@ from mininet.link import TCLink
 def myNetwork():
     net = Mininet(topo=None, build=False, ipBase='10.0.0.0/8')
 
-    info('*** Adding controller\n')
+    info(' Adding controller\n')
     c0 = net.addController(name='c0', controller=OVSController)
 
-    info('*** Add switches\n')
+    info(' Add switches\n')
     s1 = net.addSwitch('s1')
     s2 = net.addSwitch('s2')
     s3 = net.addSwitch('s3')
 
-    info('*** Add hosts\n')
-    h1 = net.addHost('h1', ip='10.0.0.1')
-    h2 = net.addHost('h2', ip='10.0.0.2')
+    info(' Add hosts\n')
+    h1 = net.addHost('h1', ip='10.0.0.1/8')
+    h2 = net.addHost('h2', ip='10.0.0.2/8')
 
-    info('*** Add links\n')
-    net.addLink(h1, s1)
-    net.addLink(s1, s2)
-    net.addLink(s2, s3)
-    net.addLink(s3, h2)
+    info(' Add links (con TCLink)\n')
+    net.addLink(h1, s1,    cls=TCLink)
+    net.addLink(s1, s2,    cls=TCLink)
+    net.addLink(s2, s3,    cls=TCLink)
 
-    info('*** Starting network\n')
+    net.addLink(s3, h2,    cls=TCLink, loss=10) 
+
+    info(' Starting network\n')
     net.build()
     c0.start()
     s1.start([c0])
     s2.start([c0])
     s3.start([c0])
+
+    info(' Ajustando MTU en s2\n')
+
+    s2.cmd('ifconfig s2-eth1 mtu 500')
 
     info('*** Running CLI\n')
     CLI(net)
