d7163881a929aa5b8b72893072424b576258db0e
Andrea Figueroa <129856581+AndreaFigueroaR@users.noreply.github.com>
2025-05-15 00:52:41 -0300

    fix(stop&wait):correct initialization

--- src/lib/protocols/stop_and_wait.py
+++ src/lib/protocols/stop_and_wait.py
@@ -3,8 +3,8 @@ from lib.protocols.selective_repeat import SelectiveRepeat
 
 class StopAndWait():
     def __init__(self, socket, remote_address):
-        self.prot_sr.set_win_size(1)
         self.prot_sr = SelectiveRepeat(socket, remote_address)
+        self.prot_sr.set_win_size(1)
 
     def send(self, data):
         self.prot_sr.send(data)
